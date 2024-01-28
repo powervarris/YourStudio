1977d4459368c81b5782481bf58b9e1af24aa167
Eksaayl <ralphangelo.llasus.cics@ust.edu.ph>
2024-01-28 14:42:06 +0800

    90% done

--- YourStudio/obj/Debug/net8.0/YourStudio.AssemblyInfo.cs
+++ YourStudio/obj/Debug/net8.0/YourStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3adf5d727a3847f5fae21033a3e6400c1a9a1903")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c86fd38f04bca5c5ba3f7d206b5f13a191e35dda")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
