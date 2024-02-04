8d7a7cf96e08b1905d6982e63cd2d56698864c2e
emsheesh69 <ejmntclr22@gmail.com>
2024-02-04 17:04:34 +0800

    "calendar"

--- YourStudio/obj/Debug/net8.0/YourStudio.AssemblyInfo.cs
+++ YourStudio/obj/Debug/net8.0/YourStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08f1be08b555e3b2ea74e6e764732d977366d8c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d40667660f5673ab727f0c3d33990e8bd74b0ddc")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
