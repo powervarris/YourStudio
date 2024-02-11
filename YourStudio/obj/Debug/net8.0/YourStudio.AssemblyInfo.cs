3f2e73fe41d9962f8ce36ae5cd9eaaafe50c4eb3
Eksaayl <144189773+Eksaayl@users.noreply.github.com>
2024-02-11 17:11:08 +0800

    w

--- YourStudio/obj/Debug/net8.0/YourStudio.AssemblyInfo.cs
+++ YourStudio/obj/Debug/net8.0/YourStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20294c9e82c7d8dba4fb5ab3edc6f4be3fe12fa4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39a23277022668b71c65ab9b862b19c32f95ec9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
