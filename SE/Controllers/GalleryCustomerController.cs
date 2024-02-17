92ea47c54d2a838448413402cb8b88297e438ec2
kenshiniro <kenssshinn@gmail.com>
2024-02-17 23:59:17 +0800

    footer nalang

---
+++ SE/Controllers/GalleryCustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SE.Controllers
+{
+    public class GalleryCustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
