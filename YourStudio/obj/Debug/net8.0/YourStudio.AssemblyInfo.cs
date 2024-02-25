861a86f341121f49d7d0a64694a2d116d95969e6
Eksaayl <144189773+Eksaayl@users.noreply.github.com>
2024-02-26 02:01:00 +0800

    Booking List for customer

--- YourStudio/obj/Debug/net8.0/YourStudio.AssemblyInfo.cs
+++ YourStudio/obj/Debug/net8.0/YourStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54328f65da2fb1c70ff302444be9f6fc054fb4b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YourStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
