7e0bf3ac65d0b6e3ee1326c53544fad13780ad98
kenshiniro <kenssshinn@gmail.com>
2024-02-28 09:43:18 +0800

    modal

---
+++ SE/Controllers/PackagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SE.Controllers
+{
+    public class PackagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
