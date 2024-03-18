4aa250d353a1e8e96ba2f59642f11818c17610a1
emsheesh69 <ejmntclr22@gmail.com>
2024-03-18 19:16:32 +0800

    new login with forget pass

---
+++ std/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using std.Models;
+using System.Diagnostics;
+
+namespace std.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Forget()
+        {
+            return View();
+        }
+        public IActionResult Otipi()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
